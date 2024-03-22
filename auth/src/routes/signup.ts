d9e46bc5f8be6c6d83dcedc9145345c04a32586c
mazhs1992 <mazhs1992@hotmail.com>
2024-03-22 11:44:32 +0200

    Error Class extensions

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,29 +1,32 @@
-import express,{Request,Response} from "express";
-import { body ,validationResult} from "express-validator";
-
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connections-error";
 
 const router = express.Router();
 
-router.post('/api/users/signup',[
-    body('email')
-        .isEmail()
-        .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({ min: 4, max: 20 })
-        .withMessage('Password must be between 4 and 20 characters')
-],(req:Request, res:Response) => {
-
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
     const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        throw new Error('Invalid email or password');
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
+    //throw new DatabaseConnectionError();
 
-    console.log('Creating user...')
-    res.send('Creating user...!');
-})
+    console.log("Creating user...");
+    res.send("Creating user...!");
+  }
+);
 
 export { router as signupRouter };
