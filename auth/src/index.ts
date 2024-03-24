f7750c5794702206ae9fe914266de0127b6b769d
mazhs1992 <mazhs1992@hotmail.com>
2024-03-24 17:08:44 +0200

    hash password

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import 'express-async-errors';
 import mongoose from "mongoose";
 import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
 
 import { currentUserRouter, } from "./routes/current-user";
 import { signoutRouter } from "./routes/signout";
@@ -11,7 +13,12 @@ import { errorHandler } from "./middleware/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
 
 app.use(currentUserRouter);
 app.use(signoutRouter);
