5fef76d77f813b7b4b5ffbae7ba8244c099a4159
mazhs1992 <mazhs1992@hotmail.com>
2024-03-24 19:57:12 +0200

    set up signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,30 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
+import { RequestValidationError } from "../errors/request-validation-error";
 
+import { User } from "../models/user";
+import jwt from "jsonwebtoken";
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi from signin!');
-})
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password").trim().notEmpty().withMessage("Password must be supplied"),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    return res.status(201).send({});
+  }
+);
 
 export { router as signinUserRouter };
