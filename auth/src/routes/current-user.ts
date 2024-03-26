a7640980a37d835ce2772bf1cbdf7d414aff830a
mazhs1992 <mazhs1992@hotmail.com>
2024-03-26 10:43:31 +0200

    refactors auth logic and error auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,9 +2,15 @@ import express from "express";
 import jwt from "jsonwebtoken";
 const router = express.Router();
 import { currentUser } from "../middleware/current-user";
+import { requireAuth } from "../middleware/require-auth";
 
-router.get("/api/users/currentuser", currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+router.get("/api/users/currentuser", 
+  currentUser, 
+  requireAuth,  
+  (req, res) => {
+
+      res.send({ currentUser: req.currentUser || null });
+      
 });
 
 export { router as currentUserRouter };
