adc6e5875b8a7334554d8c4be557e25f56b3e6c2
mazhs1992 <mazhs1992@hotmail.com>
2024-03-27 10:42:01 +0200

    sign in test

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,47 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails when a email that does not exist is supplied", async () => {
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("fails when an incorect password is applied ", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+  await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "pa1231231s",
+    })
+    .expect(400);
+});
+
+it("responds with a cookie when given a valid credential", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+  const responce = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(200);
+  expect(responce.get("Set-Cookie")).toBeDefined();
+});
