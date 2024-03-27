7fe5d9dd10a62b9795e5a42e3289fa7bee4b3dd2
mazhs1992 <mazhs1992@hotmail.com>
2024-03-27 10:03:14 +0200

    testing set up

---
+++ auth/src/app.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import 'express-async-errors';
+import mongoose from "mongoose";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+
+import { currentUserRouter, } from "./routes/current-user";
+import { signoutRouter } from "./routes/signout";
+import { signinUserRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}))
+
+app.use(currentUserRouter);
+app.use(signoutRouter);
+app.use(signinUserRouter);
+app.use(signupRouter);
+
+app.all("*",async () => {
+  throw new NotFoundError();
+})
+
+//MIDDLEWARE
+app.use(errorHandler);
+
+app.get('/api/users', (req, res) => {
+  res.send('Hi from users service!');
+})
+
+export {app}
