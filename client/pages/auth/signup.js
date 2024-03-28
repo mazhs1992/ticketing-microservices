007bf205d192363fe6816b5bdc0f26ec1df4019f
mazhs1992 <mazhs1992@hotmail.com>
2024-03-28 22:32:43 +0200

    use request logic

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import axios from "axios";
+import useRequest from "../../hooks/use-request";
+
+
+export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/users/signup",
+    method: "post",
+    body: {
+      email,
+      password,
+    },
+  })
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    doRequest()
+
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className="container-md ">
+      <form onSubmit={onSubmit}>
+        <h1>Sign Up</h1>
+        <div className="form-group mt-3 mb-3">
+          <label>Email Address</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group mt-3 mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+          />
+        </div>
+
+        {errors}
+        <button className="btn btn-primary mt-3 mb-3">Sign Up</button>
+      </form>
+    </div>
+  );
+};
