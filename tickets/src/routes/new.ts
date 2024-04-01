cd054b96971f053224c808c6cb740c376cc04a0a
mazhs1992 <mazhs1992@hotmail.com>
2024-04-01 10:21:24 +0300

    New ticket route - test

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth,validateRequest } from "@vm92tickets/common";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -15,8 +16,18 @@ requireAuth,[
         .withMessage('Price must be greater than 0')
 ],
 validateRequest,
-(req: Request, res: Response) => {
-    res.sendStatus(200)
+async (req: Request, res: Response) => {
+
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+        title,
+        price,
+        userId: req.currentUser!.id
+    })
+
+    await ticket.save()
+    res.status(201).send(ticket)
 })
 
 export {router as createTicketRouter}
