4f22dcd7dddbce9d806e716ada3339b82e12c619
mazhs1992 <mazhs1992@hotmail.com>
2024-04-01 10:42:31 +0300

    show ticket test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -6,6 +6,7 @@ import cookieSession from "cookie-session";
 
 import { errorHandler,NotFoundError,currentUser } from "@vm92tickets/common";
 import {createTicketRouter} from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.get("/api/ticketshome", (req, res) => {
   res.send("Hi from ticketsWelcome service!");
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,27 +1,35 @@
 import request from "supertest";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
+import e from "express";
 
 it('returns a 404 if ticket is not found', async () => {
+    const id = new Ticket().id
      await request(app)
-        .get('/api/tickets/kjnadwikuiib')
+        .get(`/api/tickets/${id}`)
         .send({})
         .expect(404)
   })
 
 
 it('returns the ticket if the ticket is found', async () => {
-    await request(app)
+    const title = 'concert'
+    const price = 20
+
+    const responce = await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signin())   
     .send({
-        title:'concert',
-        price: '10'
+        title,
+        price
     })
     .expect(201)
 
-    await request(app)
-       .post('/api/tickets')
-       .send({})
-       .expect(404)
+   const ticketResponce = await request(app)
+       .get(`/api/tickets/${responce.body.id}`)
+       .send()
+       .expect(200)
+
+    expect(ticketResponce.body.title).toEqual(title)
+    expect(ticketResponce.body.price).toEqual(price)
  })
