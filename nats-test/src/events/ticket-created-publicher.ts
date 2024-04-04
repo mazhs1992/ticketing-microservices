631601a511c8d3e615dd76c15d610bae70539f8d
mazhs1992 <mazhs1992@hotmail.com>
2024-04-04 10:29:22 +0300

    Publisher Class

---
+++ nats-test/src/events/ticket-created-publicher.ts
@@ -0,0 +1,7 @@
+import { Publisher } from "./base-publicer";
+import { TicketCreatedEvent } from "./ticket-created-even";
+import { Subjects } from "./subjects";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent>{
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
