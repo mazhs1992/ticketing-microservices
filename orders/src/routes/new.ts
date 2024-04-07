705b584df371929e004d262477be7624b43993d8
mazhs1992 <mazhs1992@hotmail.com>
2024-04-07 13:54:33 +0300

    Orders new route set up

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,25 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@vm92tickets/common";
+import mongoose from "mongoose";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
-router.post('/api/orders/', (req: Request, res: Response) => {
-  res.send('Orders service is up and running!');
-});
+router.post(
+  "/api/orders/",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId is required"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send("Orders service is up and running!");
+  },
+);
 
-export { router as newOrderRouter}
+export { router as newOrderRouter };
