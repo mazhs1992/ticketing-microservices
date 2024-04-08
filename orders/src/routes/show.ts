fb9777aed0c804a0e8ae8d218629ef931a4d07eb
mazhs1992 <mazhs1992@hotmail.com>
2024-04-08 10:20:20 +0300

    show order route and tests

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,32 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@vm92tickets/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+import { Order } from "../models/orders";
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send('Orders service is up and running!');
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
 
-export { router as showOrderRouter}
+    if(!order){
+      throw new NotFoundError();
+    }
+    if(order.userId !== req.currentUser!.id){
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
+  },
+);
+
+export { router as showOrderRouter };
