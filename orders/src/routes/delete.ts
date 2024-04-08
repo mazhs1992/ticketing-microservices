d25cbc9635574d7eed82d5bb2044e727b5a5c1f1
mazhs1992 <mazhs1992@hotmail.com>
2024-04-08 10:26:54 +0300

    delete order route and tests

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,33 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@vm92tickets/common";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+import { Order } from "../models/orders";
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', (req: Request, res: Response) => {
-  res.send('Orders service is up and running!');
+router.delete('/api/orders/:orderId', requireAuth,async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+  const order = await Order.findById(orderId)
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status=OrderStatus.Cancelled;
+  await order.save();
+
+  res.status(204).send(order);
+  
 });
 
 export { router as deleteOrderRouter}
