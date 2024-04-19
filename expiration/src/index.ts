2fce5253bd4b91d086768a140d747727f84b3e34
mazhs1992 <mazhs1992@hotmail.com>
2024-04-19 10:14:37 +0300

    Expiration service set up

---
+++ expiration/src/index.ts
@@ -0,0 +1,38 @@
+import { natsWrapper } from "./nats-wrapper";
+
+
+const start = async () => {
+ 
+  if (
+    !process.env.NATS_CLUSTER_ID ||
+    !process.env.NATS_CLIENT_ID ||
+    !process.env.NATS_URL
+  ) {
+    throw new Error(
+      "NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL  must be defined",
+    );
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());   
+
+    
+   
+  } catch (error: any) {
+    console.error(`Error on connection ${error.message}`);
+  }
+ console.log('Expiration service up and running')
+};
+
+start();
