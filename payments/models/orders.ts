13887e249adbc5ce693077ca2599019e017bbeeb
mazhs1992 <mazhs1992@hotmail.com>
2024-04-21 21:17:09 +0300

    Order model

---
+++ payments/models/orders.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@vm92tickets/common";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+interface OrdersAttrs {
+  id: string;
+  status: OrderStatus;
+  userId: string;
+  price: number;
+  version: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  status: OrderStatus;
+  userId: string;
+  price: number;
+  version: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrdersAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      // enum: Object.values(OrderStatus),
+      // default: OrderStatus.Created,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrdersAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
---
+++ payments/src/index.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
+import { app } from "./app";
+
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (
+    !process.env.NATS_CLUSTER_ID ||
+    !process.env.NATS_CLIENT_ID ||
+    !process.env.NATS_URL
+  ) {
+    throw new Error(
+      "NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL  must be defined",
+    );
+  }
+
+  try {
+    // Connect to NATS
+    //clusterId --> ticketing we initialize this id on infra/k8s/nats-depl.yaml
+    //clientId --> asdasd
+    //url --> http://nats-srv:4222
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error: any) {
+    console.error(`Error on connection ${error.message}`);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000  From tickets");
+  });
+};
+
+start();
